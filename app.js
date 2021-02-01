38a805867337e9dc12f4c1b9d31228437917dd8c
theata <yudengdaotian@gmail.com>
2021-02-02 00:44:10 +0900

    github認証の修正

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ passport.deserializeUser(function (obj, done) {
 passport.use(new GitHubStrategy({
   clientID: GITHUB_CLIENT_ID,
   clientSecret: GITHUB_CLIENT_SECRET,
-  callbackURL: "http://127.0.0.1:3000/auth/github/callback"
+  callbackURL: "https://polar-earth-39625.herokuapp.com/auth/github/callback"
 },
 function(accessToken, refreshToken, profile, done) {
   User.findOrCreate({ githubId: profile.id }, function (err, user) {
