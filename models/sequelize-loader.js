c87dfe2ed36de164a378fde1455141cacf204d48
theata <yudengdaotian@gmail.com>
2021-02-02 01:00:10 +0900

    seualizeの修正

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -1,4 +1,8 @@
 'use strict';
+if (process.env.DATABASE_URL) {
+    const pg = require('pg');
+    pg.defaults.ssl = true;
+  }
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
   process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/picture_arranger',
