ec5f08872ce80109fdecc324fd005c3ec5b5f5c8
theata <yudengdaotian@gmail.com>
2021-03-31 00:25:02 +0900

    認証情報の変更

--- routes/pictures.js
+++ routes/pictures.js
@@ -16,7 +16,7 @@ var uploadHandler = multer({
     autoRetry: true,
     bucket: 'ewindow-upload',
     projectId: 'ewindow',
-    keyFilename: './ewindow-06ebcfc5d100.json',
+    keyFilename: JSON.parse(ENV.fetch('GOOGLE_CREDENTIALS')),
     filename: (req, file, cb) => {
       cb(null, `${Date.now()}_${file.originalname}`);
     }
