96817d727f307c273b73a80461fe707ef196eee9
theata <yudengdaotian@gmail.com>
2021-03-30 23:59:37 +0900

    バグ修正

--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,35 @@
 var path = require('path');
 
 module.exports = {
-    context: __dirname + '/app',
-    mode: 'none',
-    entry: './entry',
-    output: {
-      path: __dirname +  './public/images',
-      filename: 'main.js'
-    },
-    module: {
-      rules: [
-        {
-            test: /\.(jpg|png|gif)$/,
-            use: [
-                {
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath : 'public/images/',
-                        publicPath : function(path){
-                            return '../' + path;
-                        }
-                    }
-                }
-            ]
-        }
-      ]
-    }
-  };
+  context: __dirname + '/app',
+  mode: 'none',
+  entry: './entry',
+  output: {
+    path: __dirname + '/public/images',
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jpg|png|gif|jpeg)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'public/images/',
+              publicPath: function (path) {
+                return '../' + path;
+              },
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
