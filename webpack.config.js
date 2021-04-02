7bed9899c7fea2317c42c773f0d11b29fdbf071b
theata <yudengdaotian@gmail.com>
2021-04-02 21:36:48 +0900

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,31 @@
 'use strict'
 var path = require('path');
 
-module.exports = {
+module.exports = 
+{
+  context: __dirname + '/app',
+  mode: 'none',
+  entry: './entry',
+  output: {
+    path: __dirname + '/public/javascripts',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+},
+{
   context: __dirname + '/app',
   mode: 'none',
   entry: './entry',
